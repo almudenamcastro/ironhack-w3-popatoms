08fe172ff6cacbaddba7b078b4aeea7b92302cc4
Gab Caser <caserogabriela@gmail.com>
2024-09-19 13:33:10 +0200

    Added temporary table and 25 aleatory songs data retrieval

--- Pop_analysis.sql
+++ Pop_analysis.sql
@@ -42,3 +42,54 @@ GROUP BY
     lustrum
 ORDER BY 
     lustrum;
+    
+-- Borrar si existe una lista de 25 canciones 
+-- aleatorias de los años en que hay chart   
+
+DROP TEMPORARY TABLE IF EXISTS random_titles;
+-- Crear la temporary table con el title solo (esto puede estar mejor hecho 
+-- para no coger canciones que no tengan weeks en el chart)
+CREATE TEMPORARY TABLE random_titles AS
+SELECT 
+    t.title
+FROM 
+    track t
+JOIN 
+    chart_entry ce ON t.track_id = ce.track_id
+JOIN 
+    chart_update cu ON ce.update_id = cu.update_id
+JOIN 
+    chart c ON cu.chart_id = c.chart_id
+WHERE 
+    cu.update_date BETWEEN '1959-01-01' AND '1967-12-31'
+ORDER BY 
+    RAND()
+LIMIT 25;
+
+-- Aquí sacamos toda esa info, y más si queremos, 
+-- si quitas MAX deja de funcionar y no te deja hacer el group by
+-- a pesar de ser valores todos iguales
+SELECT 
+    t.title, 
+    MAX(c.name) AS chart_name,  -- Usar MAX() u otra función de agregación
+    MAX(t.weeks) AS weeks,
+    MAX(t.track_id) AS track_id,
+    MAX(t.energy) AS energy,
+    MAX(t.danceability) AS danceability,
+    MAX(t.valence) AS valence,
+    MAX(t.track_mode) AS track_mode,
+    MAX(t.track_key) AS track_key
+FROM 
+    track t
+JOIN 
+    chart_entry ce ON t.track_id = ce.track_id
+JOIN 
+    chart_update cu ON ce.update_id = cu.update_id
+JOIN 
+    chart c ON cu.chart_id = c.chart_id
+JOIN 
+    random_titles rt ON t.title = rt.title
+GROUP BY 
+    t.title
+ORDER BY 
+    t.title;
